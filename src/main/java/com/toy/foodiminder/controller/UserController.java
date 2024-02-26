68e71490b33132e96aee69c5eca531b38730756c
shson <ssh5340@gmail.com>
2024-02-27 00:45:19 +0900

    [20240226] mapping url 수정

--- src/main/java/com/toy/foodiminder/controller/UserController.java
+++ src/main/java/com/toy/foodiminder/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 @RestController
 public class UserController {
 
